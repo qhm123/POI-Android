dc4c808bd637c72286c9eede00e3f24432fce707
qhm123 <qhm123@gmail.com>
2012-11-19 21:24:58 +0800

    add ppt scale

--- src/com/poi/poiandroid/MainActivity.java
+++ src/com/poi/poiandroid/MainActivity.java
@@ -1,8 +1,8 @@
 package com.poi.poiandroid;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,21 +13,31 @@ import org.apache.poi.hslf.usermodel.SlideShow;
 
 import and.awt.Dimension;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.os.Parcelable;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -35,15 +45,36 @@ public class MainActivity extends Activity {
 
 	ViewPager mViewPager;
 	PagerAdapter mPagerAdapter;
+	
+	private GestureDetector mGestureDetector;
+	private ScaleGestureDetector mScaleGestureDetector;
+
+	private boolean mPaused;
+	private boolean mOnScale = false;
+	private boolean mOnPagerScoll = false;
+	private boolean mControlsShow = false;
+	private int mPosition;
+	
+	private ProgressDialog mProgressDialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		
 		setContentView(R.layout.activity_main);
+		
+		setProgressBarVisibility(true);
+		setProgressBarIndeterminate(true);
+		setProgressBarIndeterminateVisibility(true);
 
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setPageMargin(10);
-		mViewPager.setOffscreenPageLimit(0);
+		mViewPager.setPageMarginDrawable(new ColorDrawable(Color.BLACK));
+		mViewPager.setOffscreenPageLimit(1);
+		mViewPager.setOnPageChangeListener(mPageChangeListener);
+		setupOnTouchListeners(mViewPager);
 
 		Logger.getLogger("org.teleal.cling").setLevel(Level.FINEST);
 
@@ -56,12 +87,24 @@ public class MainActivity extends Activity {
 				path = uri.getPath();
 			}
 		}
+		
+		mProgressDialog = new ProgressDialog(this);
+		mProgressDialog.setIndeterminate(true);
+		
 		try {
 			ppt2png(path);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	HashMap<Integer, View> mCache = new HashMap<Integer, View>();
+	
+	public View getView(int position) {
+		return mCache.get(position);
+	}
+	
+	int slideCount = 0;
 
 	private void ppt2png(String path) throws IOException {
 //		FileInputStream is = new FileInputStream(path);
@@ -70,36 +113,48 @@ public class MainActivity extends Activity {
 		
 		final long cur = System.currentTimeMillis();
 		
+		mProgressDialog.show();
+		
 		SlideShow ppt = new SlideShow(new File(path));
 
 		final Dimension pgsize = ppt.getPageSize();
 
 		final Slide[] slide = ppt.getSlides();
+		
+		slideCount = slide.length;
+		
+		Log.d("TIME", "new SlideShow: " + (System.currentTimeMillis() - cur));
+		
+		mProgressDialog.dismiss();
 
 		mPagerAdapter = new PagerAdapter() {
 
 			@Override
-			public void destroyItem(ViewGroup container, int position,
-					Object object) {
-				ImageView view = (ImageView) object;
-
-				container.removeView(view);
-
-				BitmapDrawable bitmapDrawable = (BitmapDrawable) view
-						.getDrawable();
-				if (!bitmapDrawable.getBitmap().isRecycled()) {
-					bitmapDrawable.getBitmap().recycle();
-				}
+			public boolean isViewFromObject(View view, Object object) {
+				return view == ((ImageView) object);
 			}
 
 			@Override
-			public Object instantiateItem(ViewGroup container, int position) {
+			public int getCount() {
+				return slide.length;
+			}
+
+			@Override
+			public void startUpdate(View container) {
+			}
+
+			@Override
+			public Object instantiateItem(View container, int position) {
+//				ImageView imageView = new ImageView(MainActivity.this);
+//				imageView.setLayoutParams(new LayoutParams(
+//						LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 				
-				Log.d("TIME", "new SlideShow: " + (System.currentTimeMillis() - cur));
-				
-				ImageView imageView = new ImageView(MainActivity.this);
+				ImageViewTouch imageView = new ImageViewTouch(
+						MainActivity.this);
 				imageView.setLayoutParams(new LayoutParams(
 						LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+				imageView.setBackgroundColor(Color.BLACK);
+				imageView.setFocusableInTouchMode(true);
 
 				String title = slide[position].getTitle();
 				System.out.println("Rendering slide " + (position + 1)
@@ -116,29 +171,327 @@ public class MainActivity extends Activity {
 				canvas.drawPaint(paint);
 
 				Graphics2D graphics2d = new Graphics2D(canvas);
-
+				
 				// render
 				slide[position].draw(graphics2d);
 
-				imageView.setImageBitmap(bmp);
+//				imageView.setImageBitmap(bmp);
+				imageView.setImageBitmapResetBase(bmp, true);
 
-				container.addView(imageView);
+				((ViewGroup) container).addView(imageView);
+				
+				mCache.put(position, imageView);
 
 				// return super.instantiateItem(container, position);
 				return imageView;
 			}
 
 			@Override
-			public boolean isViewFromObject(View view, Object object) {
-				return view == ((ImageView) object);
+			public void destroyItem(View container, int position, Object object) {
+				ImageView view = (ImageView) object;
+
+				((ViewGroup) container).removeView(view);
+
+				BitmapDrawable bitmapDrawable = (BitmapDrawable) view
+						.getDrawable();
+				if (!bitmapDrawable.getBitmap().isRecycled()) {
+					bitmapDrawable.getBitmap().recycle();
+				}
+				
+				mCache.remove(position);
 			}
 
 			@Override
-			public int getCount() {
-				return slide.length;
+			public void finishUpdate(View container) {
+			}
+
+			@Override
+			public Parcelable saveState() {
+				return null;
+			}
+
+			@Override
+			public void restoreState(Parcelable state, ClassLoader loader) {
 			}
 		};
 
 		mViewPager.setAdapter(mPagerAdapter);
+	}
+	
+	Toast mPreToast;
+	
+	ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
+		@Override
+		public void onPageSelected(int position, int prePosition) {
+			ImageViewTouch preImageView = 
+					(ImageViewTouch) getView(prePosition);
+			if (preImageView != null) {
+				preImageView.setImageBitmapResetBase(
+						preImageView.mBitmapDisplayed.getBitmap(), true);
+			}
+
+			if (getCurrentImageView() != null
+					&& getCurrentImageView().mBitmapDisplayed != null
+					&& getCurrentImageView().mBitmapDisplayed.getBitmap() == null) {
+			}
+
+			mPosition = position;
+
+//			updateZoomButtonsEnabled();
+//			updateShowInfo();
+
+			if (mPreToast != null) {
+				mPreToast.cancel();
+			}
+			mPreToast = Toast.makeText(MainActivity.this,
+					String.format("%d/%d", position + 1, slideCount),
+					Toast.LENGTH_SHORT);
+			mPreToast.show();
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset,
+				int positionOffsetPixels) {
+			// Logger.d(TAG, "onPageScrolled");
+			mOnPagerScoll = true;
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+			// Logger.d(TAG, "onPageScrollStateChanged: " + state);
+			if (state == ViewPager.SCROLL_STATE_DRAGGING) {
+				mOnPagerScoll = true;
+			} else if (state == ViewPager.SCROLL_STATE_SETTLING) {
+				mOnPagerScoll = false;
+			} else {
+				mOnPagerScoll = false;
+			}
+		}
+		
+	};
+	
+	public ImageViewTouch getCurrentImageView() {
+//		return (ImageViewTouch) mViewPager.getChildAt(mViewPager
+//				.getCurrentItem());
+		return (ImageViewTouch) getView(mViewPager.getCurrentItem());
+	}
+
+	private class MyGestureListener extends
+			GestureDetector.SimpleOnGestureListener {
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			// Logger.d(TAG, "gesture onScroll");
+			if (mOnScale) {
+				return true;
+			}
+			if (mPaused) {
+				return false;
+			}
+			ImageViewTouch imageView = getCurrentImageView();
+			if (imageView != null) {
+				imageView.panBy(-distanceX, -distanceY);
+	
+				// 超出边界效果去掉这个
+				imageView.center(true, true);
+			}
+
+			return true;
+		}
+
+		@Override
+		public boolean onSingleTapConfirmed(MotionEvent e) {
+			if (mControlsShow) {
+				// delayHideControls();
+//				hideControls();
+			} else {
+//				updateZoomButtonsEnabled();
+
+//				if (fromLocalFav) {
+//					showControls();
+//				}
+			}
+
+			return true;
+		}
+
+		@Override
+		public boolean onDoubleTap(MotionEvent e) {
+			if (mPaused) {
+				return false;
+			}
+			ImageViewTouch imageView = getCurrentImageView();
+			// Switch between the original scale and 3x scale.
+			if (imageView.mBaseZoom < 1) {
+				if (imageView.getScale() > 2F) {
+					imageView.zoomTo(1f);
+				} else {
+					imageView.zoomToPoint(3f, e.getX(), e.getY());
+				}
+			} else {
+				if (imageView.getScale() > (imageView.mMinZoom + imageView.mMaxZoom) / 2f) {
+					imageView.zoomTo(imageView.mMinZoom);
+				} else {
+					imageView.zoomToPoint(imageView.mMaxZoom, e.getX(),
+							e.getY());
+				}
+			}
+
+//			updateZoomButtonsEnabled();
+			return true;
+		}
+	}
+
+	private class MyOnScaleGestureListener extends
+			ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+		float currentScale;
+		float currentMiddleX;
+		float currentMiddleY;
+
+		@Override
+		public void onScaleEnd(ScaleGestureDetector detector) {
+
+//			updateZoomButtonsEnabled();
+
+			final ImageViewTouch imageView = getCurrentImageView();
+
+			Log.d(TAG, "currentScale: " + currentScale + ", maxZoom: "
+					+ imageView.mMaxZoom);
+			if (currentScale > imageView.mMaxZoom) {
+				imageView
+						.zoomToNoCenterWithAni(currentScale
+								/ imageView.mMaxZoom, 1, currentMiddleX,
+								currentMiddleY);
+				currentScale = imageView.mMaxZoom;
+				imageView.zoomToNoCenterValue(currentScale, currentMiddleX,
+						currentMiddleY);
+			} else if (currentScale < imageView.mMinZoom) {
+				imageView.zoomToNoCenterWithAni(currentScale,
+						imageView.mMinZoom, currentMiddleX, currentMiddleY);
+				currentScale = imageView.mMinZoom;
+				imageView.zoomToNoCenterValue(currentScale, currentMiddleX,
+						currentMiddleY);
+			} else {
+				imageView.zoomToNoCenter(currentScale, currentMiddleX,
+						currentMiddleY);
+			}
+
+			imageView.center(true, true);
+
+			// NOTE: 延迟修正缩放后可能移动问题
+			imageView.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					mOnScale = false;
+				}
+			}, 300);
+			// Logger.d(TAG, "gesture onScaleEnd");
+		}
+
+		@Override
+		public boolean onScaleBegin(ScaleGestureDetector detector) {
+			// Logger.d(TAG, "gesture onScaleStart");
+			mOnScale = true;
+			return true;
+		}
+
+		@Override
+		public boolean onScale(ScaleGestureDetector detector, float mx, float my) {
+			// Logger.d(TAG, "gesture onScale");
+			ImageViewTouch imageView = getCurrentImageView();
+			float ns = imageView.getScale() * detector.getScaleFactor();
+
+			currentScale = ns;
+			currentMiddleX = mx;
+			currentMiddleY = my;
+
+			if (detector.isInProgress()) {
+				imageView.zoomToNoCenter(ns, mx, my);
+			}
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent m) {
+		if (mPaused)
+			return true;
+		// delayHideControls();
+		return super.dispatchTouchEvent(m);
+	}
+	
+	private void setupOnTouchListeners(View rootView) {
+		mGestureDetector = new GestureDetector(this, new MyGestureListener(),
+				null, true);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
+			mScaleGestureDetector = new ScaleGestureDetector(this,
+					new MyOnScaleGestureListener());
+		}
+
+		OnTouchListener rootListener = new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				// NOTE: gestureDetector may handle onScroll..
+				if (!mOnScale) {
+					if (!mOnPagerScoll) {
+						try {
+							mGestureDetector.onTouchEvent(event);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
+					if (!mOnPagerScoll) {
+						mScaleGestureDetector.onTouchEvent(event);
+					}
+				}
+
+				ImageViewTouch imageView = getCurrentImageView();
+				if (!mOnScale && imageView != null
+						&& imageView.mBitmapDisplayed != null
+						&& imageView.mBitmapDisplayed.getBitmap() != null) {
+					Matrix m = imageView.getImageViewMatrix();
+					RectF rect = new RectF(0, 0, imageView.mBitmapDisplayed
+							.getBitmap().getWidth(), imageView.mBitmapDisplayed
+							.getBitmap().getHeight());
+					m.mapRect(rect);
+					// Logger.d(TAG, "rect.right: " + rect.right +
+					// ", rect.left: "
+					// + rect.left + ", imageView.getWidth(): "
+					// + imageView.getWidth());
+					// 图片超出屏幕范围后移动
+					if (!(rect.right > imageView.getWidth() + 0.1 && rect.left < -0.1)) {
+						try {
+							mViewPager.onTouchEvent(event);
+						} catch (Exception e) {
+							// why?
+							e.printStackTrace();
+						}
+					}
+				}
+
+				// We do not use the return value of
+				// mGestureDetector.onTouchEvent because we will not receive
+				// the "up" event if we return false for the "down" event.
+				return true;
+			}
+		};
+
+		rootView.setOnTouchListener(rootListener);
+	}
+	
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		mPaused = false;
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		mPaused = true;
 	}
 }
